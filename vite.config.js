72b4801a82aeb4bd798f0fe706ad1436a3e963c4
peter <peter@pitedeMacBook-Pro.local>
2026-05-04 13:05:38 +0700

    测试

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/v3-vite/',
+    // base: '/v3-vite/',
+    base: '/',
     plugins: [vue()],
     server: {
         host: '0.0.0.0',
